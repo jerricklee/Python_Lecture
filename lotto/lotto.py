887dec470149957af96309301e3e89ca10f48a66
jerricklee <jerrick_@naver.com>
2015-08-26 16:16:39 +0900

    no message

--- lotto/lotto.py
+++ lotto/lotto.py
@@ -1,1 +1,16 @@
 __author__ = 'jerrick'
+
+import random
+
+def makeLotto():
+    numList = list(range(1, 47))
+    random.shuffle(numList)
+    # print(numList)
+    return (numList[0:6], numList[6])
+
+def main():
+    lottoList = makeLotto()
+    print("6 lotto number : ", lottoList[0])
+    print("number for Second : ", lottoList[1])
+if __name__ == '__main__':
+    main()
